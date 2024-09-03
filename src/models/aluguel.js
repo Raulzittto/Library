157c8539471a3b8188911ed8c7e191de3be91927
Raul <soaresraul22@gmail.com>
2024-09-03 09:20:07 -0300

    feat: add crud model

---
+++ src/models/aluguel.js
@@ -0,0 +1,41 @@
+const livros_controller = require("../controllers/livros.js");
+const estudante_controller = require("../controllers/estudante.js");
+
+let proxId = 1;
+
+const data_aluguel = body.data_aluguel
+.replaceAll("-", "")
+.replaceAll("/", "")
+.replaceAll(".", "")
+.replaceAll(" ", "")
+
+const data_devolucao = body.data_devolucao
+.replaceAll("-", "")
+.replaceAll("/", "")
+.replaceAll(".", "")
+.replaceAll(" ", "")
+
+
+const model = (body, id = proxId++) => {
+  if (
+    livros_controller.show(body.livro_ID)&&
+    estudante_controller.show(body.estudante_ID)&&
+    data_aluguel != undefined &&
+    data_aluguel.lenght >= 6 &&
+    data_aluguel.lenght <= 8 &&
+    devolucao != undefined &&
+    devolucao.lenght >= 6 &&
+    devolucao.lenght <= 8
+
+  ) {
+    return {
+      id,
+      livro_ID: body.livro_ID,
+      estudante_ID: body.estudante_ID,
+      data_aluguel: data_aluguel,
+      data_devolucao: data_devolucao,
+    };
+  }
+};
+
+module.exports = model;
